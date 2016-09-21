1a81471256df392f902618ff487c130947b0a779
Eugene Morgunov <evmorgunov@yahoo.com>
2016-09-21 18:48:35 +0300

    no_multithreading

---
+++ Loops/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // WhileLoopExample();
+            WhileLoopExample();
+            CheckForInput();
+            
+        }
+
+        static void WhileLoopExample()
+        {
+            string userIsDone = "";
+            //	Test	on	a	lower-class	copy	of	the	string.
+            while (userIsDone.ToLower()	!=	"yes")
+            {				Console.WriteLine("In	while	loop");
+                Console.Write("Are	you	done?	[yes]	[no]:	");
+                userIsDone	=	Console.ReadLine();
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void CheckForInput()
+        {
+            Task mytask = Task.Run(() =>
+            {
+                int [] repeats = new int[5] { 1, 2, 3, 4, 5 };
+                foreach (int item in repeats)
+                {
+                    Console.WriteLine(item);
+                    Thread.Sleep(1000);
+                }
+               
+            });
+            mytask.Wait();            
+        }
+
+
+
+    }
+}
