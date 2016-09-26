fe1dc399777ab1deb6bfd406c4ea262bf2a811f8
Eugene Morgunov <evmorgunov@yahoo.com>
2016-09-26 21:32:45 +0300

    GEtUnderlyingtype&typeof on enum

---
+++ FunWithEnums/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithEnums
+{
+    class Program
+    {
+        public enum Consumption : int // enums are user-defined data types that can contain any pre-defined values
+        { 
+            Vedro = 10, // this enum uses int for its inner data storage
+            Hammer = 30,
+            Logan = 7
+        }
+
+        static void Main(string[] args)
+        {
+            // var result = GetConsumption(Consumption.Vedro);
+            //  Console.WriteLine(result);
+
+            Consumption ee = Consumption.Hammer; // make an instance of object ee  of type "consumption"
+         //   GetEnumTypeViaGetType(ee); // invoking method to get type 
+            GetEnumTypeViaTypeOf(ee);
+        }
+
+        public static string GetConsumption(Consumption ee) // passing object of type "consumption" ee as a parameter
+        {
+            switch (ee)
+            {
+                case Consumption.Vedro:                    
+                    return "A fair one";                    
+                case Consumption.Hammer:
+                    return "You can't afford that";                    
+                case Consumption.Logan:
+                    return "Perfect!";                    
+                default:
+                    return "Wrong Input";
+            }            
+        }
+
+        public static void GetEnumTypeViaGetType(Consumption ee) // getting underlying type of enum via .GetUnderlyingType() and GetType() methods
+        {     // returns the type which was used to build enum data structure      
+            Console.WriteLine(Consumption.GetUnderlyingType(ee.GetType())); 
+            /* 1) Taking object "ee" of type "consumption" as argument 
+             * 1) Getting the type of object "ee":   ee.GetType()
+             * 2) passing the result as argument to Consumption.GetUnderlyingType(), and getting the underlying type 
+             *
+             * !!!! GetType() needs to be cast on the instance of an object, while typeof() doesn't
+             */           
+        }
+
+        public static void GetEnumTypeViaTypeOf(Consumption ee) // getting underlying type of enum via .GetUnderlyingType() and GetType() methods
+        {
+            Type t = Consumption.GetUnderlyingType(typeof(Consumption)); // .typeof() takes type as argument (not object of type)
+            Console.WriteLine(t);            
+        }
+
+    }
+}
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -10,13 +10,13 @@ namespace MethodOverloading
     {
         static void Main(string[] args)
         {
-            FunWithObjects();
+            FunWithMatrix();
         }
 
         static void FunWithObjects()
         {
             int[] integers = new int[2] { 2, 3 }; // simple array of integers (not used in example)
-            object[] stuff = new object[4] {1, false, "Hitler", 3}; // array of objects
+            object[] stuff = new object[4] { 1, false, "Hitler", 3 }; // array of objects
 
             foreach (var obj in stuff)
             {
@@ -24,5 +24,29 @@ namespace MethodOverloading
             }
         }
 
+        static void FunWithMatrix() // also known as two-dimension rectangular array 
+        {
+            int[,] currency = new int[2,3]; // represents a square structure, where [numner of vertical lines, number of horizontal lines]
+            currency[0, 0] = 1;
+            currency[0, 1] = 2;
+            currency[0, 2] = 3;
+            currency[1, 0] = 4;
+            currency[1, 1] = 5;
+            currency[1, 2] = 6;                
+
+            for (int i = 0; i < 2; i++) // here we use <, not <=, because 1st index in arrays is addressed as 0, not 1
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    
+                     Console.WriteLine(currency[i,j]);
+                }
+            }
+        }
+
+        static void FunWithMultiDimArrays()
+        {
+
+        }
     }
 }
