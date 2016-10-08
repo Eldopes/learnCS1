c2b79448db4fa81e7dd7182074be8b8c695c14d3
Eugene Morgunov <evmorgunov@yahoo.com>
2016-10-08 12:48:15 +0300

    some more json fun

--- HomeServices/Controllers/CalcController.cs
+++ HomeServices/Controllers/CalcController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using HomeServices.Models; // using custom model class "Consumption"
+using System.Collections.Specialized;
+using System.Web.Script.Serialization; // for json serialization
+using Newtonsoft.Json.Linq; // new library for json parsing 
+
 
 namespace HomeServices.Controllers
 {
@@ -18,15 +22,19 @@ namespace HomeServices.Controllers
         public ActionResult GetCost(FuelInfo fuel_info) // using custom model class "Consumption"
         {
             object to_calc = fuel_info; // save data for passing to CalculateCost() method
-            CalculateCost(fuel_info); // invokes outer calculation method
-
-            object result = fuel_info;
-            return Json(result); // successfully returning the result to view (TEST)
+            JsonResult result = CalculateCost(to_calc); // invokes calculation method and seves the result of type "JsonResult"
+            return result; // successfully returning the result of type "JsonResult" to view (TEST)
         }
 
-        private double CalculateCost() // TODO: Object from GetCost() to array and processing 
+        private JsonResult CalculateCost(object to_calc) // TODO: JSon parsing via JSON.net and calculation 
         {
-            return 1;
+            string data_json = new JavaScriptSerializer().Serialize(to_calc); // getting json from object
+            JObject elements = JObject.Parse(data_json); // parsing json to get key-value pairs 
+                        
+            //  var value = GetValue(elements);
+            //   Jobject  smth like this  
+
+            return Json(to_calc); // returns the result of calculation to GetCost method 
         }
 
     }
