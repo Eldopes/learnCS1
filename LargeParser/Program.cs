6ebf4351559b2e1002f3ded57c9decbe8ae880a8
Eldopes <evmorgunov@yahoo.com>
2016-10-28 17:09:53 +0300

    Added: - Parser - Large Parser - Memory Detector (from MSDN)

---
+++ LargeParser/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace LargeParser
+{
+    public class GlobalData
+    {
+        public static Dictionary<string, int> parcedResults = new Dictionary<string, int>(); // global dictionary for storing the result
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input stream:
+            try
+            {
+             //   using (StreamReader sr = File.OpenText("C:\\Users\\Eugene\\OneDrive\\war_and_peace.txt"))
+                using (StreamReader sr = File.OpenText("D:\\Downloads\\log\\log.txt"))
+                {
+                    Console.OutputEncoding = Encoding.Unicode;
+                    StringBuilder input = new StringBuilder();
+
+                    while (!sr.EndOfStream)
+                    {
+                        input = input.Append(sr.ReadLine().ToLower()); // append the stream-read line to the stringbuilder                            
+                        Parse(input);
+                        input.Clear();
+                    }
+                }
+                }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            // Output stream:
+            List<KeyValuePair<string, int>> sortedResults = (from pair in GlobalData.parcedResults orderby pair.Value descending select pair).ToList(); // sorting the dictionary and packing it to list 
+
+            try
+            {
+               //   using (StreamWriter sw = new StreamWriter("C:\\Users\\Eugene\\OneDrive\\parse_result\\result_new.txt"))
+                using (StreamWriter sw = new StreamWriter("D:\\Downloads\\log\\log_result.txt"))
+                {
+
+                    foreach (KeyValuePair<string, int> kv in sortedResults)
+                    {
+                        sw.WriteLine("{0} => {1}", kv.Key, kv.Value);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot write ot file:");
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+
+        static void Parse(StringBuilder input)
+        {
+             
+             string pattern = "[\\p{P}+|\\s+|\\t|\\n|\\r]"; // pattern for regular expression parsing (punctuation marks + tabulation + Next line + carriage return)
+             string[] splitted = Regex.Split(input.ToString(), pattern);
+            
+            foreach (var item in splitted)
+            {
+                if (GlobalData.parcedResults.ContainsKey(item))
+                {
+                    GlobalData.parcedResults[item]++; // if contains => increment existing value by 1                                   
+                } 
+                else if (!string.IsNullOrEmpty(item) /* to remove null values */ ) 
+                {
+                    GlobalData.parcedResults.Add(item, 1); // if not => add with a value of 0    
+                } 
+            }                  
+           
+        }
+    }
+}
--- WordParser/UltimateReturner.cs
+++ WordParser/UltimateReturner.cs
@@ -5,24 +5,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.Diagnostics; // for tracking process memory usage
 
 namespace WordParser
 {
-    class Program
+    class UltimateReturner
     {
 
         static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.Unicode; // for unicode Console support 
-            string input = "";
-            
+
+            Console.OutputEncoding = Encoding.Unicode; 
+            string input = string.Empty;            
+
             // Open the text file using a stream reader.
-                try
-                {   
-                    using (StreamReader sr = new StreamReader("C:\\Users\\Eugene\\OneDrive\\fck_peace.txt", Encoding.GetEncoding("Unicode"))) 
-                    {                        
-                        input = sr.ReadToEnd(); // Read the stream to a string
-                }
+            try
+                {
+                  using (StreamReader sr = new StreamReader("C:\\Users\\Eugene\\OneDrive\\war_and_peace.txt", Encoding.GetEncoding("Unicode")))
+              //  using (StreamReader sr = new StreamReader("D:\\Downloads\\log\\log.txt", Encoding.GetEncoding("Unicode")))
+                    {
+
+                    input = sr.ReadToEnd().ToLower(); // Read the stream to a string
+
+                    }
                 }
                 catch (Exception e)
                 {
@@ -33,14 +38,14 @@ namespace WordParser
             // Write to  the text file using a stream writer.
                 try
                 {
-                    using (StreamWriter sw = new StreamWriter("C:\\Users\\Eugene\\OneDrive\\parse_result\\result.txt"))
-                    {
-                        foreach (KeyValuePair<string, int> kv in Parse(input))
-                        {
-                            sw.WriteLine("{0} =>  {1}", kv.Key, kv.Value);
+                using (StreamWriter sw = new StreamWriter("C:\\Users\\Eugene\\OneDrive\\parse_result\\result.txt"))               
+                {
 
-                        }
+                    foreach (KeyValuePair<string, int> kv in Parse(input))
+                    {
+                        sw.WriteLine("{0} => {1}", kv.Key, kv.Value);
                     }
+                }
                 }
                 catch (Exception e)
                 {
@@ -52,6 +57,7 @@ namespace WordParser
         static IDictionary<string, int> Parse(string input)
         {            
             string pattern = "[\\p{P}+|\\s+|\\t|\\n|\\r]"; // pattern for regular expression parsing (punctuation marks + tabulation + Next line + carriage return)          
+
             string [] splitted = Regex.Split(input, pattern);
             Dictionary<string, int> parcedResults = new Dictionary<string, int>(); // new dictionary for storing the parsed results
 
@@ -65,9 +71,9 @@ namespace WordParser
               {
                 parcedResults.Add(line.Metric, line.Count); // adding parced results to our dictionary (already sorted in the right way)
               }
+            parcedResults.Remove(""); // removing null values 
+
             return parcedResults;
         }
-
-
     }
 }
