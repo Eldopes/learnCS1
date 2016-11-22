376ba07a39eefae17941e486f4669e8919dc1b7b
Eugene Morgunov <evmorgunov@yahoo.com>
2016-11-22 21:39:05 +0300

    Log function

---
+++ LogCalculating/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogCalculating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Pow(20,8));
+
+            Console.WriteLine(Math.Pow(16000,2));
+
+            Console.WriteLine(Math.Log10(Math.Pow(16000, 2)));
+        }
+    }
+}
